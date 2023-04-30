8beecdcaa1162eef62b93a2b1d96f6db874a806d
barbara scalise <barbaranahas@gmail.com>
2023-04-30 11:17:46 -0300

    agregue array

--- while.js
+++ while.js
@@ -1,16 +1,6 @@
-/*let i = 1;
+let i = 1;
 
 while (i <= 5) {
   console.log(i);
   i++;
-}*/
-
-let password = "";
-
-do {
-  password = prompt("Ingrese su contraseña : ");
-  if (password !== "secreto") {
-    alert("Contraseña incorrecta,intente nuevamente");
-  }
-} while (password !== "secreto");
-alert("Bienvenido!");
+}
